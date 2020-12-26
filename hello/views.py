088be9b62c8a067ce0d4eae937e8b8821beba971
nathanjchan <njchan@ucdavis.edu>
2020-12-26 00:26:13 -0800

    Test user input and submit

--- hello/views.py
+++ hello/views.py
@@ -17,3 +17,17 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+
+def search(request):
+    if request.method == "POST":
+        longitude = request.POST.get("longitude", None)
+        latitude = request.POST.get("latitude", None)
+        total = longitude + latitude
+        if total < 10:
+            return HttpResponse("Your search button worked, and the total is less than 10.")
+        elif total >= 10:
+            return HttpResponse("Your search button worked, and the total is 10 or more.")
+        else:
+            return HttpResponse("Your search button worked, but there was no total.")
+    return HttpResponse("Your search button worked, but nothing happened.")
