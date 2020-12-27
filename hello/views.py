1a692b18e9db7d2729f97d59263117ae89f0c008
nathanjchan <njchan@ucdavis.edu>
2020-12-26 16:33:00 -0800

    Add radar image search

--- hello/views.py
+++ hello/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import numpy as np
+import pandas as pd
+import math
+
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
@@ -21,18 +25,46 @@ def db(request):
 
 def search(request):
     if request.method == "POST":
-        try:
-            longitude = float(request.POST.get("longitude", None))
-            latitude = float(request.POST.get("latitude", None))
-            total = longitude + latitude
-            if total < 10:
-                html = "<html><body>The total is less than 10.</body></html>"
-                return HttpResponse(html)
-            else:
-                html = "<html><body>The total is 10 or more.</body></html>"
-                return HttpResponse(html)
-        except:
-            html = "<html><body>There was some error.</body></html>"
-            return HttpResponse(html)
-    html = "<html><body>Nothing happened.</body></html>"
+        longitude = float(request.POST.get("longitude", None))
+        latitude = float(request.POST.get("latitude", None))
+
+        df = pd.read_csv("https://github.com/nathanjchan/ice-on-mars/raw/master/radar2.csv")
+        df = df.drop(columns="Unnamed: 0")
+
+        min_dist = float("inf")
+        min_tif = ""
+        min_depth = 0
+        min_long = 0
+        min_lat = 0
+        for index, row in df.iterrows():
+            dist = math.dist([longitude, latitude], [row["center_long"], row["center_lat"]])
+            if dist < min_dist:
+                min_dist = dist
+                min_tif = row["tif"]
+                min_depth = row["depth"]
+
+                min_long = row["center_long"]
+                min_lat = row["center_lat"]
+
+                min_tif = min_tif.split("Radar_Images/tiff/")[1]
+                min_tif = min_tif.split("tiff.tif")[0]
+                min_jpg = "".join(["https://pds-geosciences.wustl.edu/mro/mro-m-sharad-5-radargram-v1/mrosh_2001/browse/thm/", min_tif, "thm.jpg"])
+
+        if min_depth == -32768:
+            depth_message = "<p>According to the model from Piqueux et al. 2019, there is no ice on this location.</p>"
+        else:
+            depth_message = "".join(["<p>According to the model from Piqueux et al. 2019, there is ice ", str(min_depth), " cm under the surface at this location.</p>"])
+
+        html_list = [
+            "<html><body><center>",
+            "<p>Here is a radar image at {longitude: ", str(min_long), ", latitude: ", str(min_lat), "},</p>",
+            "<p>which is ", str(min_dist), " units away from {longitude: ", str(longitude), ", latitude: ", str(latitude), "}.</p>",
+            depth_message,
+            "<p>The image is from the Geosciences Node of the NASA Planetary Data System.</p>"
+            '<p><img src="', min_jpg, '" alt="radar image"></p>',
+            "</center></body><html>"
+        ]
+        html = "".join(html_list)
+        return HttpResponse(html)
+    html = "<html><body>There was some error and nothing happened.</body></html>"
     return HttpResponse(html)
