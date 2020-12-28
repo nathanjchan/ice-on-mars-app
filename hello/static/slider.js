53234e2db784c67a714a95cb776a405abe45c863
nathanjchan <njchan@ucdavis.edu>
2020-12-27 20:18:16 -0800

    Change to slider input

---
+++ hello/static/slider.js
@@ -0,0 +1,7 @@
+var longitudeSlider = document.getElementById("longitudeSlider");
+var latitudeSlider = document.getElementById("latitudeSlider");
+var longitudeValue = document.getElementById("longitudeValue");
+var latitudeValue = document.getElementById("latitudeValue");
+
+longitudeValue.innerHTML = longitudeSlider.value;
+latitudeValue.innerHTML = latitudeSlider.value;
