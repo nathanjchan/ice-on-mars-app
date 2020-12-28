51c4308a200ee00f42dbc693ecbb92728e4d1f43
nathanjchan <njchan@ucdavis.edu>
2020-12-27 17:29:54 -0800

    Remove pickle import

--- hello/utilities.py
+++ hello/utilities.py
@@ -1,19 +1,4 @@
-import pickle
 import numpy as np
-
-
-def save_object(self, obj, file_name):
-    """Given an object and a file name, save the item to local memory using pickle"""
-    with open(file_name, "wb") as output:
-        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
-    return
-
-
-def load_object(self, file_name):
-    """Given a file name, load an item from local memory using pickle, and return the object"""
-    with open(file_name, "rb") as input:
-        obj = pickle.load(input)
-    return obj
 
 
 def distance(x1, y1, x2, y2):
