b13080d49dd78614e35933337f43c33d441fb12c
nathanjchan <njchan@ucdavis.edu>
2020-12-27 17:15:18 -0800

    Add nav bar, and searching text

---
+++ hello/templates/search.js
@@ -0,0 +1,8 @@
+function startSearch() {
+    var text = document.getElementById("searching");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
--- hello/views.py
+++ hello/views.py
@@ -5,6 +5,7 @@ from .models import Greeting
 
 import numpy as np
 import pandas as pd
+from .utilities import distance
 
 # Create your views here.
 def index(request):
@@ -22,14 +23,10 @@ def db(request):
     return render(request, "db.html", {"greetings": greetings})
 
 
-def distance(x1, y1, x2, y2):
-    return np.sqrt(np.sum((x1-x2)**2 + (y1-y2)**2))
-
-
 def search(request):
     if request.method == "POST":
-        longitude = float(request.POST.get("longitude", None))
-        latitude = float(request.POST.get("latitude", None))
+        longitude = int(request.POST.get("longitude", None))
+        latitude = int(request.POST.get("latitude", None))
 
         df = pd.read_csv("https://github.com/nathanjchan/ice-on-mars/raw/master/radar2.csv")
         df = df.drop(columns="Unnamed: 0")
@@ -54,20 +51,20 @@ def search(request):
                 min_jpg = "".join(["https://pds-geosciences.wustl.edu/mro/mro-m-sharad-5-radargram-v1/mrosh_2001/browse/thm/", min_tif, "thm.jpg"])
 
         if min_depth == -32768:
-            depth_message = "<p>According to the model from Piqueux et al. 2019, there is no ice on this location.</p>"
+            depth_message = '<p>According to the model from <a href="https://doi.org/10.1029/2019GL083947">Piqueux et al. 2019</a>, there is no ice on this location.</p>'
         else:
-            depth_message = "".join(["<p>According to the model from Piqueux et al. 2019, there is ice ", str(min_depth), " cm under the surface at this location.</p>"])
+            depth_message = "".join(['<p>According to the model from <a href="https://doi.org/10.1029/2019GL083947">Piqueux et al. 2019</a>, there is ice ', str(min_depth), " cm under the surface at this location.</p>"])
 
         html_list = [
+            '{% extends "base.html" %}{% load static %}{% block content %}',
             "<html><body><center>",
             "<p>Here is a radar image at {longitude: ", str(min_long), ", latitude: ", str(min_lat), "},</p>",
-            "<p>which is ", str(min_dist), " units away from {longitude: ", str(longitude), ", latitude: ", str(latitude), "}.</p>",
+            "<p>which is ", str(round(min_dist, 2)), " units away from {longitude: ", str(longitude), ", latitude: ", str(latitude), "}.</p>",
             depth_message,
-            "<p>The image is from the Geosciences Node of the NASA Planetary Data System.</p>"
+            '<p>The image is from the <a href="https://pds-geosciences.wustl.edu/">Geosciences Node of the NASA Planetary Data System</a>.</p>',
             '<p><img src="', min_jpg, '" alt="radar image" height="900"></p>',
-            "</center></body><html>"
+            "</center></body><html>",
+            "{% endblock %}"
         ]
         html = "".join(html_list)
         return HttpResponse(html)
-    html = "<html><body>There was some error and nothing happened.</body></html>"
-    return HttpResponse(html)
