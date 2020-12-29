6e10f8f5c53f5bb537aaef63ee94033993eeb138
nathanjchan <njchan@ucdavis.edu>
2020-12-29 14:31:35 -0800

    Update search.js

--- hello/static/search.js
+++ hello/static/search.js
@@ -6,13 +6,13 @@ var searchText3 = document.getElementById("searchText3");
 searchButton.onclick = function() {
   while (true) {
     searchText1.style.display = "block";
-    setTimeout(function(){return;}, 1000);
+    setTimeout(function(){}, 1000);
     searchText1.style.display = "none";
     searchText2.style.display = "block";
-    setTimeout(function(){return;}, 1000);
+    setTimeout(function(){}, 1000);
     searchText2.style.display = "none";
     searchText3.style.display = "block";
-    setTimeout(function(){return;}, 1000);
+    setTimeout(function(){}, 1000);
     searchText3.style.display = "none";
   }
 }
