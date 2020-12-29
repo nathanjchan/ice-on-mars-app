ff302b83f0809b50e7372ad086d65e038edcfcf4
nathanjchan <njchan@ucdavis.edu>
2020-12-29 14:11:24 -0800

    Try animated search text

--- hello/static/search.js
+++ hello/static/search.js
@@ -1,10 +1,28 @@
 var searchButton = document.getElementById("searchButton");
-var searchText = document.getElementById("searchText");
+var searchText1 = document.getElementById("searchText1");
+var searchText2 = document.getElementById("searchText2");
+var searchText3 = document.getElementById("searchText3");
+
+check();
 
 searchButton.onclick = function() {
-    if (searchText.style.display == "none") {
-      searchText.style.display = "block";
+  while (true) {
+    if (searchText1.style.display == "none") {
+      searchText1.style.display = "block";
     } else {
-      searchText.style.display = "none";
+      searchText1.style.display = "none";
+    }
+    setTimeout(check, 1000);
+    if (searchText2.style.display == "none") {
+      searchText2.style.display = "block";
+    } else {
+      searchText2.style.display = "none";
+    }
+    setTimeout(check, 1000);
+    if (searchText3.style.display == "none") {
+      searchText3.style.display = "block";
+    } else {
+      searchText3.style.display = "none";
     }
   }
+}
