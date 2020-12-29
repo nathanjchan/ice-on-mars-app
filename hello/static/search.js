d5eacca3bdf707028127030b5ff21285031f9052
nathanjchan <njchan@ucdavis.edu>
2020-12-29 14:27:30 -0800

    Update search.js

--- hello/static/search.js
+++ hello/static/search.js
@@ -3,26 +3,16 @@ var searchText1 = document.getElementById("searchText1");
 var searchText2 = document.getElementById("searchText2");
 var searchText3 = document.getElementById("searchText3");
 
-check();
-
 searchButton.onclick = function() {
   while (true) {
-    if (searchText1.style.display == "none") {
-      searchText1.style.display = "block";
-    } else {
-      searchText1.style.display = "none";
-    }
-    setTimeout(check, 1000);
-    if (searchText2.style.display == "none") {
-      searchText2.style.display = "block";
-    } else {
-      searchText2.style.display = "none";
-    }
-    setTimeout(check, 1000);
-    if (searchText3.style.display == "none") {
-      searchText3.style.display = "block";
-    } else {
-      searchText3.style.display = "none";
-    }
+    searchText1.style.display = "block";
+    setTimeout(function(){return;}, 1000);
+    searchText1.style.display = "none";
+    searchText2.style.display = "block";
+    setTimeout(function(){return;}, 1000);
+    searchText2.style.display = "none";
+    searchText3.style.display = "block";
+    setTimeout(function(){return;}, 1000);
+    searchText3.style.display = "none";
   }
 }
