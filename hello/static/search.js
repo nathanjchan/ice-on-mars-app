52b6da735cf8b61e689a20f1aa49c9e508dc2126
nathanjchan <njchan@ucdavis.edu>
2020-12-29 15:13:47 -0800

    Get rid of animated search text

--- hello/static/search.js
+++ hello/static/search.js
@@ -1,18 +1,10 @@
 var searchButton = document.getElementById("searchButton");
-var searchText1 = document.getElementById("searchText1");
-var searchText2 = document.getElementById("searchText2");
-var searchText3 = document.getElementById("searchText3");
+var searchText = document.getElementById("searchText");
 
 searchButton.onclick = function() {
-  while (true) {
-    searchText1.style.display = "block";
-    setTimeout(function(){}, 1000);
-    searchText1.style.display = "none";
-    searchText2.style.display = "block";
-    setTimeout(function(){}, 1000);
-    searchText2.style.display = "none";
-    searchText3.style.display = "block";
-    setTimeout(function(){}, 1000);
-    searchText3.style.display = "none";
+    if (searchText.style.display == "none") {
+      searchText.style.display = "block";
+    } else {
+      searchText.style.display = "none";
+    }
   }
-}
