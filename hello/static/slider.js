aa5507fe28fb1b2759cd631663a663d902eebc3b
nathanjchan <njchan@ucdavis.edu>
2020-12-28 18:46:59 -0800

    Test x again

--- hello/static/slider.js
+++ hello/static/slider.js
@@ -7,8 +7,10 @@ longitudeValue.innerHTML = longitudeSlider.value;
 latitudeValue.innerHTML = latitudeSlider.value;
 
 var x = document.getElementById("x");
+x.style.position = "absolute"
 x.style.top = "50%"
 x.style.bottom = "50%"
+x.style.transform = "translate(-50%, -50%)"
 
 longitudeSlider.oninput = function() {
     longitudeValue.innerHTML = this.value;
