5cc3eac36e51e63a727fa8fabfeec95b975def68
nathanjchan <njchan@ucdavis.edu>
2020-12-29 00:20:04 -0800

    Update slider.js

--- hello/static/slider.js
+++ hello/static/slider.js
@@ -10,14 +10,18 @@ var x = document.getElementById("x");
 x.style.position = "absolute";
 x.style.top = "50%"
 x.style.left = "50%"
-x.style.transform = "translate(-50%, -50%)";
+x.style.transform = "translate(-50%, -60%)";
 
 longitudeSlider.oninput = function() {
     longitudeValue.innerHTML = this.value;
-    x.style.left = (((this.value - 180) / 360) * 100) + "%";
+    if (this.value > 180) {
+        x.style.left = ((this.value - 180) / 360) * 100 + "%";
+    } else {
+        x.style.left = ((this.value + 180) / 360) * 100 + "%";
+    }
 }
 
 latitudeSlider.oninput = function() {
     latitudeValue.innerHTML = this.value;
-    x.style.top = ((-(this.value - 90) / 180) * 100) + "%";
+    x.style.top = (-(this.value - 90) / 180) * 100 + "%";
 }
